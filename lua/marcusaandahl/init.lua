f1b576693bca0f6d1df31d851747592f94aa0731
Marcus Andreas Aandahl <marcus.aandahl@lego.com>
2026-07-01 13:03:18 +0200

    Format all files

--- lua/marcusaandahl/init.lua
+++ lua/marcusaandahl/init.lua
@@ -11,7 +11,7 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.opt.wrap = false -- Don't wrap text
+vim.opt.wrap = false         -- Don't wrap text
 vim.opt.incsearch = true
 vim.opt.termguicolors = true -- Needed for theme
 
@@ -19,17 +19,17 @@ vim.opt.termguicolors = true -- Needed for theme
 vim.cmd.colorscheme "catppuccin-mocha"
 
 -- LSP SETTINGS
-vim.lsp.enable("lua_ls") -- brew install lua-language-server
-vim.lsp.enable("jdtls") -- brew install jdtls
-vim.lsp.enable("ts_ls") -- npm install -g typescript-language-server typescript
-vim.lsp.enable("bashls") -- npm i -g bash-language-server
-vim.lsp.enable("tailwindcss") -- npm install -g @tailwindcss/language-server
-vim.lsp.enable("svelte") -- npm install -g svelte-language-server
-vim.lsp.enable("rust_analyzer") -- brew install rust-analyzer
-vim.lsp.enable("pyright") -- npm install -g pyright
+vim.lsp.enable("lua_ls")                 -- brew install lua-language-server
+vim.lsp.enable("jdtls")                  -- brew install jdtls
+vim.lsp.enable("ts_ls")                  -- npm install -g typescript-language-server typescript
+vim.lsp.enable("bashls")                 -- npm i -g bash-language-server
+vim.lsp.enable("tailwindcss")            -- npm install -g @tailwindcss/language-server
+vim.lsp.enable("svelte")                 -- npm install -g svelte-language-server
+vim.lsp.enable("rust_analyzer")          -- brew install rust-analyzer
+vim.lsp.enable("pyright")                -- npm install -g pyright
 vim.lsp.enable("docker_language_server") -- go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
-vim.lsp.enable("cssmodules_ls") -- npm install -g cssmodules-language-server
-vim.lsp.enable("css_variables") -- npm i -g css-variables-language-server
+vim.lsp.enable("cssmodules_ls")          -- npm install -g cssmodules-language-server
+vim.lsp.enable("css_variables")          -- npm i -g css-variables-language-server
 
 
 -- ENABLE HIGHLIGHTING
--- lua/marcusaandahl/lazy/telescope.lua
+++ lua/marcusaandahl/lazy/telescope.lua
@@ -1,7 +1,8 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons'
-    }
+  'nvim-telescope/telescope.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons'
+  }
 }
