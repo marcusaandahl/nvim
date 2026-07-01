5a2e3fa5673f37e7b6135c6c07e59dd6b9b10441
Marcus Andreas Aandahl <marcus.aandahl@lego.com>
2026-07-01 13:01:24 +0200

    Add copilot

--- lua/marcusaandahl/init.lua
+++ lua/marcusaandahl/init.lua
@@ -55,5 +55,18 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function() vim.treesitter.start() end,
 })
 
+-- COPILOT HIGHLIGHTING
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = 'solarized',
+  -- group = ...,
+  callback = function()
+    vim.api.nvim_set_hl(0, 'CopilotSuggestion', {
+      fg = '#555555',
+      ctermfg = 8,
+      force = true
+    })
+  end
+})
+
 -- KEY MAPPINGS
 require("marcusaandahl.keys")
